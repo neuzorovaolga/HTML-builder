2fe463c832adf516d7009ae20cb8698627027541
Nevzorova Olga <neuzorova@gmail.com>
2024-01-25 09:23:41 +0300

    refactor: copy directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,7 +1,25 @@
 const fs = require('fs');
 const fsPromises = require('fs').promises;
 
-fs.mkdir('04-copy-directory/files-copy', () => {});
+fs.mkdir('04-copy-directory/files-copy', (err) => {
+  if (err) {
+    fs.readdir('04-copy-directory/files-copy', (err, files) => {
+      files.forEach((item) => {
+        fs.unlink(`04-copy-directory/files-copy/${item}`, () => {});
+      });
+      files.forEach((item) => {
+        fsPromises
+          .copyFile(
+            `04-copy-directory/files/${item}`,
+            `04-copy-directory/files-copy/${item}`,
+          )
+          .catch(function (error) {
+            console.log(error);
+          });
+      });
+    });
+  }
+});
 
 fs.readdir('04-copy-directory/files', (err, files) => {
   if (err) throw err;
